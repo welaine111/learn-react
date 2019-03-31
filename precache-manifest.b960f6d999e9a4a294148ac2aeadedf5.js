15bd22da2cbc223adc11387ff1d900a0e4a45468
Yang Wang <Yang.Wang2@arm.com>
2019-03-31 13:30:47 -0700

    Updates

---
+++ precache-manifest.b960f6d999e9a4a294148ac2aeadedf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f5a4aa29338276e39d8",
+    "url": "/learn-react/static/js/runtime~main.1bcabeff.js"
+  },
+  {
+    "revision": "22ed3bf51f97091071e3",
+    "url": "/learn-react/static/js/main.8777367f.chunk.js"
+  },
+  {
+    "revision": "0e721810f69c4e16dd50",
+    "url": "/learn-react/static/js/2.a46c82bc.chunk.js"
+  },
+  {
+    "revision": "22ed3bf51f97091071e3",
+    "url": "/learn-react/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "0e721810f69c4e16dd50",
+    "url": "/learn-react/static/css/2.b848e1c4.chunk.css"
+  },
+  {
+    "revision": "9a3ef1cea7b0c5f14a9d17037fdf47ac",
+    "url": "/learn-react/index.html"
+  }
+];
